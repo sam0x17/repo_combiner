ba9fbd283f94667bb36be84433785d7758428b97
Sam Johnson <sam@durosoft.com>
2022-02-15 03:41:55 -0500

    add code for download_repo method

--- src/repo_combiner.cr
+++ src/repo_combiner.cr
@@ -1,10 +1,22 @@
+require "file_utils"
+
 class RepoCombiner
+  property target_dir : String
+
   def initialize
-    puts unique_dir
+    @target_dir = unique_dir
   end
 
-  def download_repo(url : String, branch : String) : String
-    dest_dir = "/tmp/#{Random::Secure.hex(8)}"
+  def download_repo(url, target_dir, branch = nil)
+    raise "target_dir cannot be found!" unless File.exists?(target_dir)
+    puts "Cloning into #{url}..."
+    FileUtils.mkdir_p(target_dir)
+    puts `git clone '#{url}' '#{target_dir}'`
+    raise ".git directory cannot be found after cloning!" unless File.exists?("#{target_dir}/.git")
+    if branch
+      puts "Checking out into #{branch} branch..."
+      puts `git checkout #{branch}`
+    end
   end
 
   private def unique_dir
