a7cb47077924b7283275e5ea59c312624ddf6d95
Sam Johnson <sam@durosoft.com>
2022-02-15 03:11:26 -0500

    basic setup, ability to make a unique directory name

--- src/repo_combiner.cr
+++ src/repo_combiner.cr
@@ -1,6 +1,19 @@
-# TODO: Write documentation for `RepoCombiner`
-module RepoCombiner
-  VERSION = "0.1.0"
 
-  # TODO: Put your code here
+class RepoCombiner
+  def initialize
+    puts unique_dir
+  end
+
+  def download_repo(url : String, branch : String) : String
+    dest_dir = "/tmp/#{Random::Secure.hex(8)}"
+  end
+
+  private def unique_dir
+    loop do
+      dest_dir = "/tmp/#{Random::Secure.hex(8)}"
+      return dest_dir unless File.exists?(dest_dir)
+    end
+  end
 end
+
+combiner = RepoCombiner.new
