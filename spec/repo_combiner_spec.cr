283105213dfd364e1c789b907646ed8a0c953ab5
Sam Johnson <sam@durosoft.com>
2022-02-17 01:22:46 -0500

    initialize target_dir and add verbose mode

--- spec/repo_combiner_spec.cr
+++ spec/repo_combiner_spec.cr
@@ -1,6 +1,12 @@
 require "./spec_helper"
 
 describe RepoCombiner do
+  it "initializes the target directory with a blank repo" do
+    combiner = RepoCombiner.new
+    File.exists?("#{combiner.target_dir}/.git").should eq true
+    FileUtils.rm_rf(combiner.target_dir)
+  end
+
   it "can download a repo" do
     combiner = RepoCombiner.new
     dir = combiner.download_repo("https://github.com/sam0x17/repo_combiner.git")
--- src/repo_combiner.cr
+++ src/repo_combiner.cr
@@ -2,23 +2,34 @@ require "file_utils"
 
 class RepoCombiner
   property target_dir : String
+  property verbose : Bool = false
 
   def initialize
     @target_dir = unique_dir
+    FileUtils.mkdir_p(@target_dir)
+    git_cmd "git init #{@target_dir}"
+    raise "could not initialize git repo" unless File.exists?("#{@target_dir}/.git")
   end
 
   def download_repo(url, dir = nil, branch = nil)
     dir ||= unique_dir
     FileUtils.mkdir_p(dir)
     raise "dir cannot be found!" unless File.exists?(dir)
-    puts "Cloning into #{url}..."
-    puts `git clone '#{url}' '#{dir}'`
+    git_cmd "git clone '#{url}' '#{dir}'"
     raise ".git directory cannot be found after cloning!" unless File.exists?("#{dir}/.git")
     if branch
-      puts "Checking out into #{branch} branch..."
-      puts `git checkout #{branch}`
+      git_cmd "git checkout #{branch}"
     end
     dir
+  end
+
+  private def vputs(msg)
+    puts msg if @verbose
+  end
+
+  private def git_cmd(cmd)
+    vputs cmd
+    vputs `#{cmd} #{@verbose ? "" : "--quiet"}`
   end
 
   private def unique_dir
